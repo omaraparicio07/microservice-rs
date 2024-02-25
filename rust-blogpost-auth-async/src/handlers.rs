10c04d967051ef0a771f5d2b833f1a4b3cd884df
OmarApQz <omaraparicio07@gmail.com>
2024-02-25 11:27:47 -0600

    coding Add handler for register user into db

--- rust-blogpost-auth-async/src/handlers.rs
+++ rust-blogpost-auth-async/src/handlers.rs
@@ -1,17 +1,87 @@
-use actix_web::Responder;
+use crate::{
+    jwt_auth,
+    model::{ LoginUserSchema, RegisterUserSchema, TokenClaims, User },
+    response::FilteredUser,
+    AppState,
+};
+use actix_web::{
+    cookie::{ time::Duration as ActixWebDuration, Cookie },
+    get,
+    post,
+    web,
+    HttpMessage,
+    HttpRequest,
+    HttpResponse,
+    Responder,
+};
+use argon2::{
+    password_hash::{ rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString },
+    Argon2,
+};
+use chrono::{ prelude::*, Duration };
+use jsonwebtoken::{ encode, EncodingKey, Header };
+use serde_json::json;
+use sqlx::Row;
 
-pub async fn get_users() -> impl Responder {
-    format!("hello from get users")
+fn filter_user_record(user: &User) -> FilteredUser {
+    FilteredUser {
+        id: user.id.to_string(),
+        email: user.email.to_owned(),
+        name: user.name.to_owned(),
+        photo: user.photo.to_owned(),
+        role: user.role.to_owned(),
+        verified: user.verified,
+        createdAt: user.created_at.unwrap(),
+        updatedAt: user.updated_at.unwrap(),
+    }
 }
 
-pub async fn get_user_by_id() -> impl Responder {
-    format!("hello from get users by id")
-}
+#[post("/auth/register")]
+async fn register_user_handler(
+    body: web::Json<RegisterUserSchema>,
+    data: web::Data<AppState>
+) -> impl Responder {
+    let exists: bool = sqlx
+        ::query("SELECT EXISTS(SELECT 1 FROM users WHERE email = $1)")
+        .bind(body.email.to_owned())
+        .fetch_one(&data.db).await
+        .unwrap()
+        .get(0);
 
-pub async fn add_user() -> impl Responder {
-    format!("hello from add user")
-}
+    if exists {
+        return HttpResponse::Conflict().json(
+            serde_json::json!({"status": "fail","message": "User with that email already exists"})
+        );
+    }
 
-pub async fn delete_user() -> impl Responder {
-    format!("hello from delete user")
+    let salt = SaltString::generate(&mut OsRng);
+    let hashed_password = Argon2::default()
+        .hash_password(body.password.as_bytes(), &salt)
+        .expect("Error while hashing password")
+        .to_string();
+    let query_result = sqlx
+        ::query_as!(
+            User,
+            "INSERT INTO users (name,email,password) VALUES ($1, $2, $3) RETURNING *",
+            body.name.to_string(),
+            body.email.to_string().to_lowercase(),
+            hashed_password
+        )
+        .fetch_one(&data.db).await;
+
+    match query_result {
+        Ok(user) => {
+            let user_response =
+                serde_json::json!({"status": "success","data": serde_json::json!({
+                "user": filter_user_record(&user)
+            })});
+
+            return HttpResponse::Ok().json(user_response);
+        }
+        Err(e) => {
+            return HttpResponse::InternalServerError().json(
+                serde_json::json!({"status": "error","message": format!("{:?}", e)})
+            );
+        }
+    }
 }
