b3832b42db003eafd8e6560becd26a251d324841
OmarApQz <omaraparicio07@gmail.com>
2024-03-10 11:11:15 -0600

    coding Add handler for logout, destroy jwt

--- rust-blogpost-auth-async/src/handlers.rs
+++ rust-blogpost-auth-async/src/handlers.rs
@@ -137,6 +137,19 @@ async fn login_user_handler(
         .json(json!({"status": "success", "token": token}))
 }
 
+#[get("/auth/logout")]
+async fn logout_handler(_: jwt_auth::JwtMiddleware) -> impl Responder {
+    let cookie = Cookie::build("token", "")
+        .path("/")
+        .max_age(ActixWebDuration::new(-1, 0))
+        .http_only(true)
+        .finish();
+
+    HttpResponse::Ok()
+        .cookie(cookie)
+        .json(json!({"status": "success"}))
+}
+
 #[get("/users/me")]
 async fn get_me_handler(
     req: HttpRequest,
