b79760e6e10a0a5ff203ad98aad55cf694364b82
OmarApQz <omaraparicio07@gmail.com>
2024-03-10 11:10:22 -0600

    coding Add handler for login user, generate jwt

--- rust-blogpost-auth-async/src/handlers.rs
+++ rust-blogpost-auth-async/src/handlers.rs
@@ -86,6 +86,57 @@ async fn register_user_handler(
     }
 }
 
+#[post("/auth/login")]
+async fn login_user_handler(
+    body: web::Json<LoginUserSchema>,
+    data: web::Data<AppState>,
+) -> impl Responder {
+    let query_result = sqlx::query_as!(User, "SELECT * FROM users WHERE email = $1", body.email)
+        .fetch_optional(&data.db)
+        .await
+        .unwrap();
+
+    let is_valid = query_result.to_owned().map_or(false, |user| {
+        let parsed_hash = PasswordHash::new(&user.password).unwrap();
+        Argon2::default()
+            .verify_password(body.password.as_bytes(), &parsed_hash)
+            .map_or(false, |_| true)
+    });
+
+    if !is_valid {
+        return HttpResponse::BadRequest()
+            .json(json!({"status": "fail", "message": "Invalid email or password"}));
+    }
+
+    let user = query_result.unwrap();
+
+    let now = Utc::now();
+    let iat = now.timestamp() as usize;
+    let exp = (now + Duration::minutes(60)).timestamp() as usize;
+    let claims: TokenClaims = TokenClaims {
+        sub: user.id.to_string(),
+        exp,
+        iat,
+    };
+
+    let token = encode(
+        &Header::default(),
+        &claims,
+        &EncodingKey::from_secret(data.env.jwt_secret.as_ref()),
+    )
+    .unwrap();
+
+    let cookie = Cookie::build("token", token.to_owned())
+        .path("/")
+        .max_age(ActixWebDuration::new(60 * 60, 0))
+        .http_only(true)
+        .finish();
+
+    HttpResponse::Ok()
+        .cookie(cookie)
+        .json(json!({"status": "success", "token": token}))
+}
+
 #[get("/healthchecker")]
 async fn health_checker_handler() -> impl Responder {
     const MESSAGE: &str = "JWT Authentication in Rust using Actix-web, Postgres, and SQLX";
